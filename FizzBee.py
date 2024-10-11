f8b2fd986a95269c4fb1d50c7a9e22cb8a938b22
AMZIANE Ryles <12201506@crit-xubuntu>
2024-10-11 12:25:15 +0200

    new file:   FizzBee.py

---
+++ FizzBee.py
@@ -0,0 +1,24 @@
+import unittest
+from fizzbuzz import  affiche
+
+class testfizzbee(unittest.TestCase):
+        def test_affiche(self):
+            output = affiche()
+            excepted_output = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
+            self.assertEqual(output[:45], excepted_output)
+if __name__ == '__main__':
+        unittest.main()
+
+def affiche():
+    result = []
+    for i in range (1, 101):
+        if i % 15 == 0:
+            result.append("FrisBee")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return ''.join(result)
+    
